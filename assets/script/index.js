0c67de86201909bc8ec3397b15603d134ff31b91
Shaun <117479605+LamingerShaun@users.noreply.github.com>
2022-11-18 14:34:20 -0600

    first JS implementations

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const getTimeString = ({ hours, minutes, seconds, zone }) => {
+    if (minutes / 10 < 1) {
+        minutes = "0" + minutes;
+    }
+    if (seconds / 10 < 1) {
+        seconds = "0" + seconds;
+    }
+    return `${hours}:${minutes}:${seconds} ${zone}`;
+};
+
+const renderTime = () => {
+    var currentTime = document.getElementById("current-time");
+    const currentDate = new Date();
+    var hours = currentDate.getHours();
+    var minutes = currentDate.getMinutes();
+    var seconds = currentDate.getSeconds();
+    var zone = hours >= 12 ? "PM" : "AM";
+    if (hours > 12) {
+    hours = hours % 12;
+    }
+    const timeString = getTimeString({ hours, minutes, seconds, zone });
+    currentTime.innerHTML = timeString;
+};
+
+
+setInterval(renderTime, 1000);
